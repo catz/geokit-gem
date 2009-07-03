fdebb17f09dd0c3fba6a2b6eb160dec4728aa8fa
Nick <nick@grayinvader.(none)>
2009-07-03 20:02:57 +0400

    adds yandex(maps.yandex.ru) geocoder

--- lib/geokit/geocoders.rb
+++ lib/geokit/geocoders.rb
@@ -70,6 +70,7 @@ module Geokit
     @@timeout = nil    
     @@yahoo = 'REPLACE_WITH_YOUR_YAHOO_KEY'
     @@google = 'REPLACE_WITH_YOUR_GOOGLE_KEY'
+    @@yandex = 'REPLACE_WITH_YOUR_YANDEX_KEY'
     @@geocoder_us = false
     @@geocoder_ca = false
     @@geonames = false
@@ -325,6 +326,60 @@ module Geokit
         rescue 
           logger.info "Caught an error during Yahoo geocoding call: "+$!
           return GeoLoc.new
+      end
+    end
+
+    class YandexGeocoder < Geokit::Geocoders::Geocoder
+
+      @@country_mappings = {
+        'Россия' => 'RU',
+        'США' => 'US',
+        'Великобритания' => 'GB',
+        'Беларусь' => 'BY',
+        'Украина' => 'UA',
+        #TODO: add countries
+      }
+      
+      private
+
+      # Template method which does the geocode lookup.
+      def self.do_geocode(address, options = {})
+        address_str = address.is_a?(GeoLoc) ? address.to_geocodeable_s : address
+        url="http://geocode-maps.yandex.ru/1.x/?key=#{Geokit::Geocoders::yandex}&geocode=#{Geokit::Inflector::url_escape(address_str)}"
+        res = self.call_geocoder_service(url)
+        return GeoLoc.new if !res.is_a?(Net::HTTPSuccess) && !res.is_a?(Net::HTTPMovedPermanently)
+
+        xml = res.body
+        doc = REXML::Document.new(xml)
+        logger.debug "Yandex geocoding. Address: #{address}. Result: #{xml}"
+
+        if doc.elements['//GeocoderResponseMetaData/found'].text.to_i != 0
+          res=GeoLoc.new
+
+          loc = doc.elements['//GeoObject']
+
+          #basic
+          res.full_address = loc.elements['.//text'].text
+          res.lng, res.lat = loc.elements['.//Point/pos'].text.split(' ')
+          res.country_code = @@country_mappings[loc.elements['.//CountryName'].text]
+          res.provider='yandex'
+
+          #extended - false if not available
+          res.city=loc.elements['.//LocalityName'].text if loc.elements['.//LocalityName'] && loc.elements['.//LocalityName'].text != nil
+          res.state=loc.elements['.//AdministrativeAreaName'].text if loc.elements['.//AdministrativeAreaName'] && loc.elements['.//AdministrativeAreaName'].text != nil
+          res.street_address=loc.elements['.//ThoroughfareName'].text if loc.elements['.//ThoroughfareName'] && loc.elements['.//ThoroughfareName'].text != nil
+          res.accuracy=loc.elements['.//precision'].text if loc.elements['.//precision'] && loc.elements['.//precision'].text != nil
+          res.suggested_bounds = Bounds.normalize(loc.elements['.//Envelope/lowerCorner'].text.split(' ').reverse, loc.elements['.//Envelope/upperCorner'].text.split(' ').reverse)
+          res.success=true
+          return res
+        else
+          logger.info "Yahoo was unable to geocode address: "+address
+          return GeoLoc.new
+        end
+
+      rescue
+        logger.info "Caught an error during Yandex geocoding call: "+$!
+        return GeoLoc.new
       end
     end
 
